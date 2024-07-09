a85549fe97ad5f92332cb320c2a28903f8e0e99c
Yuri <yurifl03@gmail.com>
2024-07-09 14:03:17 -0300

    wip

--- home/.config/fish/config.fish
+++ home/.config/fish/config.fish
@@ -10,6 +10,8 @@ fish_add_path (go env GOPATH)/bin
 set -Ux USE_GKE_GCLOUD_AUTH_PLUGIN True
 set -Ux GOPATH /opt/homebrew/bin/go
 set -Ux NIX_GIT_CHECKS false
+set -Ux AWS_REGION us-east-1
+set -Ux AWS_PROFILE nsx
 
 alias bundle="brew bundle --file=~/.config/Brewfile --cleanup"
 alias z="zellij"
