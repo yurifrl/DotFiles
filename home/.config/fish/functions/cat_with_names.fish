3c2049aa02812b680b0590534f93c1e1b42dc1c5
Yuri <yurifl03@gmail.com>
2024-07-28 15:47:16 -0300

    refactor(cat_with_names.fish): refactor function to improve readability and modularity by splitting logic into smaller functions feat(cat_with_names.fish): add support for processing directories and their files in addition to single files for better flexibility

--- home/.config/fish/functions/cat_with_names.fish
+++ home/.config/fish/functions/cat_with_names.fish
@@ -1,11 +1,26 @@
-function cat_with_names
-    for filename in $argv
-        if test -f $filename
-            echo "# $filename"
-            cat $filename
-        else
-            echo "File $filename does not exist."
-        end
+function read_and_print_file --argument file_path
+    if test -f $file_path
+        echo "# $file_path"
+        cat $file_path
+    else
+        echo "Could not read file $file_path"
     end
 end
 
+function process_path --argument path
+    if test -f $path
+        read_and_print_file $path
+    else if test -d $path
+        for file in (find $path -type f)
+            read_and_print_file $file
+        end
+    else
+        echo "Path does not exist: $path"
+    end
+end
+
+function cat_with_names --argument path
+    for arg in $argv
+        process_path $arg
+    end
+end
