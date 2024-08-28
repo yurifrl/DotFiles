2d1d6d5b9fff980d8cfd9435feb0609af2428a83
Yuri <yurifl03@gmail.com>
2024-08-28 09:02:11 -0300

    :rocket:

--- home/.config/fish/config.fish
+++ home/.config/fish/config.fish
@@ -5,13 +5,18 @@ fish_add_path ~/.local/bin
 fish_add_path ~/.bin
 fish_add_path ~/.cargo/env
 fish_add_path (go env GOPATH)/bin
+fish_add_path ~/.nix-profile/bin
 
 
 set -Ux USE_GKE_GCLOUD_AUTH_PLUGIN True
 set -Ux GOPATH /opt/homebrew/bin/go
+set -Ux GOMODCACHE $HOME/go/pkg/mod
 set -Ux NIX_GIT_CHECKS false
 set -Ux AWS_REGION us-east-1
 set -Ux AWS_PROFILE nsx
+set -Ux KUBE_EDITOR nvim
+# set -Ux OCO_EMOJI true
+# set -Ux OCO_ONE_LINE_COMMIT true
 
 alias bundle="brew bundle --file=~/.config/Brewfile --cleanup"
 alias z="zellij"
@@ -24,6 +29,7 @@ alias v="nvim"
 alias vf="vim ~/.config/fish/config.fish"
 alias vi="nivm"
 alias vim="nvim"
+alias neovim="nvim"
 alias x="exit"
 
 function ip-gateway
